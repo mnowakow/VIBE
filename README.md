07cb709387630acda82e1cb9675d20dc77b9fc44
mnowakow <matthias.nowakowski@gmail.com>
2021-08-16 18:03:15 +0200

    Changed to js, created npmignore

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # VerovioScoreEditor
+
+![npm](https://img.shields.io/npm/v/@nowams/verovioscoreeditor)
 npm package for a score editor using verovio as rendering engine
+
+
--- src/entries/h5p-verovio-notation.js
+++
@@ -1,8 +0,0 @@
-import "../styles/h5p-verovio-notation.css";
-import VerovioNotation from "../scripts/js/h5p-verovio-notation.js";
-
-// Load library
-H5P = H5P || {};
-H5P.VerovioNotation = VerovioNotation;
-
-//test here
